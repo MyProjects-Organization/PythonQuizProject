60cd70ca26abec0988a9c34a3303f597f079d627
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2022-09-24 16:27:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 import random
 #
 question_bank = []
@@ -11,10 +12,20 @@ for question in question_data:
 # print(question_bank)
 # print(question_bank[0].text)
 
-for i in range(0,4):
-    # print(random.randint(0,11))
-    rand = random.randint(0,11)
-    print(question_bank[rand].text, question_bank[rand].answer)
+quiz = QuizBrain(question_bank)
+# This line: "print(quiz)" will output <quiz_brain.QuizBrain object at 0x7f9a9fce2070>
+print(quiz)
+# This line: "quiz.next_question()" will output Q. 1: A slug's blood is green. (True/False)?
+quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+# for i in range(0,4):
+#     # print(random.randint(0,11))
+#     rand = random.randint(0,11)
+#     print(question_bank[rand].text, question_bank[rand].answer)
 
 # list = [20, 30, 40, 50 ,60, 70, 80]
 # sampling = random.choices(list, k=4)
