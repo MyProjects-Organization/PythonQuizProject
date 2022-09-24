be0a7665562f9d62f8f6e55a1b85da0c317fca46
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2022-09-24 16:29:29 +0800

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,3 +3,17 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+
+    def still_has_questions(self):
+        # if self.question_number < len(self.question_list):
+        #     return True
+        # else:
+        #     False
+    #  The codes above can be replaced by the code below because it will be evaluated either as True or False,
+        #  so the return statement can be used as a replacement
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f'Q. {self.question_number}: {current_question.text} (True/False)?\n')
